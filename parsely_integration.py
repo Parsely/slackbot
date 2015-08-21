82d5be273ac5c2b3350b703b7e75b102f9903e42
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-08-21 17:59:53 -0400

    started adding arguments for versatility

--- parsely_integration.py
+++ parsely_integration.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
+import argparse
 import json
 import requests
 from config import *
 
-
+# let's add some parser arguments
+parser = argparse.ArgumentParser(description='call Parsely API and send top post results to Slack!'
+parser.add_argument('--mention', nargs="*", help="usernames to mention")
+parser.add_argument('--num', nargs="*", type=int, default=5, help="number of posts to return")
+args = parser.argparse()
+print args
 url = "https://api.parsely.com/v2/analytics/posts?apikey={}&page=1&limit=5&sort=_hits".format(APIKEY)
 api_results = requests.get(url)
 payload = {'channel': CHANNEL, "username": "Parselybot", "text": "Top 5 posts last 24 hours"}

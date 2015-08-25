b9819ee4fc59465abd522739cce3a79355ee5140
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-08-25 08:28:06 -0400

    fixed some minor bugs

--- parsely_integration.py
+++ parsely_integration.py
@@ -7,20 +7,25 @@ from config import *
 # let's add some parser arguments
 parser = argparse.ArgumentParser(description='call Parsely API and send top post results to Slack!')
 parser.add_argument('--mention', nargs="*", help="usernames to mention")
-parser.add_argument('--num', nargs=1, default=5, help="number of posts to return")
+parser.add_argument('--num', default=5, help="number of posts to return")
 parser.add_argument('--no-thumbs', action='store_const', const="no_thumbs", help="don't get thumbnails")
-parser.add_argument('--shares', action='store_const', const="shares", help="get share counts for posts (will take longer due to multiple API requests!")
+parser.add_argument('--shares', action='store_const', const="shares", 
+                    help="get share counts for posts")
 args = parser.parse_args()
 
 mentions = ''
-for name in args.mention:
-    mentions += "@{} ".format(name)
-url = "https://api.parsely.com/v2/analytics/posts?apikey={}&page=1&limit={}&sort=_hits".format(APIKEY, args.num[0])
+if args.mention:
+    for name in args.mention:
+        mentions += "@{} ".format(name)
+url = "https://api.parsely.com/v2/analytics/posts?apikey={}&page=1&limit={}&sort=_hits".format(APIKEY, args.num)
+print url
 api_results = requests.get(url)
+# The "text" field here appears to be unused when rich messaging is used as below,
+# but is apparently still necessary
 payload = {'channel': CHANNEL, "username": "Parselybot", "text": "Top 5 posts last 24 hours"}
 attachments = []
-intro_text =  {"fallback": "{}: Top {} Posts In Past 1 Days".format(mentions, args.num[0]), 
-                "pretext": "{}: Top {} Posts In 24 Hours".format(mentions, args.num[0])}
+intro_text =  {"fallback": "{}: Top {} Posts In Past 1 Days".format(mentions, args.num), 
+                "pretext": "{}: Top {} Posts In 24 Hours".format(mentions, args.num)}
 attachments.append(intro_text)
 for entry in api_results.json()['data']:
     fields = [{'title': "Author: {}".format(entry['author']), 
@@ -39,9 +44,11 @@ for entry in api_results.json()['data']:
                         'short': "true"}
         fields.append(shares_dict)
     temp_dict['fields'] = fields
-        
     attachments.append(temp_dict)
 json_payload = {'payload': payload, 'attachments': attachments}
 
-
-r = requests.post(WEBHOOK_URL, data=json.dumps(json_payload)) 
+# take our json data and send it up
+try:
+    requests.post(WEBHOOK_URL, data=json.dumps(json_payload))
+except requests.exceptions.MissingSchema:
+    print "The webhook URL appears to be invalid. Are you sure you have the right webhook URL?"

f6c558461c02dd939d975e6f3ba9fd309ed0744b
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-09-19 20:26:07 -0400

    started adding flask slack stuff

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_slack import Slack
+from config import *
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-        return 'Hello World!'
+slack = Slack(app)
+
+@slack.command('parsely', token=ACCESS_TOKEN, 
+        team_id=None, methods]['POST']) 
+def parsely(**kwargs):
+    text = kwargs.get('text')
+
+app.add_url_rule('/', view_func=slack.dispatch)
+
 
     if __name__ == '__main__':
             app.run()

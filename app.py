99667fd485781117b0808ae6c893d3a1da2b7b39
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-10-30 15:51:23 -0400

    added framework for accepting slack commands

--- app.py
+++ app.py
@@ -9,10 +9,35 @@ slack = Slack(app)
                 ['POST']) 
 def parsely(**kwargs):
     text = kwargs.get('text')
-    print text
+    commands = [word.strip() for word in text.strip().split(',')]
+    if 'author' or 'post' or 'section' or 'tag' in commands[0]:
+        analytics(commands)
+    elif shares:
+        shares(commands)
+    elif referrers:
+        referrers(commands)
+    elif realtime(commands):
+        realtime(commands)
+    return slack.response("Sorry, didn't recognize the command!")
+
+def analytics(command_list):
+    # TODO
+    pass
+
+def shares(command_list):
+    # TODO
+    pass
+
+def referrers(command_list):
+    # TODO
+    pass
+
+def realtime(command_list):
+    # TODO
+    pass
 
 app.add_url_rule('/', view_func=slack.dispatch)
 
 
 if __name__ == '__main__':
-        app.run('0.0.0.0', 6000)
+        app.run('0.0.0.0', 6000, debug=True)

03f37b558df4f06383cbafbedc9af07eee65562c
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-11-06 13:35:28 -0500

    beginning to add slackbot stuff

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask
 from flask_slack import Slack
 from config import *
+import parsely_slack
 app = Flask(__name__)
 
 slack = Slack(app)
-
+apikey = ""
+secret = ""
+slackbot = parsely_slack.ParselySlack(apikey, secret)
 @slack.command('parsely', 'DcJRP8Lr9NRcjrQqAFoNQm2K', 'T090APE76',
                 ['POST']) 
 def parsely(**kwargs):
     text = kwargs.get('text')
     commands = [word.strip() for word in text.strip().split(',')]
     if 'author' or 'post' or 'section' or 'tag' in commands[0]:
-        analytics(commands)
+        slackbot.analytics(commands)
     elif shares:
-        shares(commands)
+        slackbot.shares(commands)
     elif referrers:
-        referrers(commands)
+        slackbot.referrers(commands)
     elif realtime(commands):
-        realtime(commands)
+        slackbot.realtime(commands)
     return slack.response("Sorry, didn't recognize the command!")
 
 def analytics(command_list):
-    # TODO
+    slackbot.
     pass
 
 def shares(command_list):
--- parsely_slack.py
+++ parsely_slack.py
@@ -1,76 +1,103 @@
-from __future__ import unicode_literals
-import argparse
-import json
 import requests
-import sys
-from config import *
+import datetime
+from datetime import datetime as dt
+from parsely.parsely import Parsely
+import re
 
-# let's add some parser arguments
-parser = argparse.ArgumentParser(
-        description='call Parsely API and send top post results to Slack!')
-parser.add_argument(
-    'api_type', help='realtime or analytics') 
-parser.add_argument(
-    '--mention', nargs='*', help='usernames to mention')
-parser.add_argument(
-    '--limit', default=5, help='number of records to return')
-parser.add_argument(
-    '--no-thumbs', action='store_const', const='no_thumbs', 
-    help="don't get thumbnails")
-parser.add_argument(
-    '--shares', action='store_const', const='shares', 
-    help='get share counts for posts')
-parser.add_argument('--time', default='1h', 
-    help=('Realtime only: number of minutes or hours for time period,' 
-    'up to 24h. Examples of valid values: 5m, 15m, 1h, 12h.')
-parser.add_argument(
-    '--days', default=3, 
-    help='analytics only: number of days to count hits for (Default: 3)')
-args = parser.parse_args()
 
-mentions = ''
-if args.mention:
-    for name in args.mention:
-        mentions += '@{} '.format(name)
-url = ROOT_URL + '{}/posts?apikey={}&secret={}page=1&limit={}&sort=_hits&time={}&days={}'.format(
-                    args.api_type, APIKEY, SHARED_SECRET, args.limit, args.time, args.days)
-api_results = requests.get(url)
-if api_results.json().get('success') is False:
-    sys.exit("Couldn't get API results. Are you sure your shared secret is correct?")
-# The 'text' field here appears to be unused when rich messaging is used as below,
-# but is apparently still necessary
-payload = {'channel': CHANNEL, 'username': 'Parselybot', 'text': 'Top 5 posts last 24 hours'}
-attachments = []
-if args.api_type == 'analytics':
-    intro_text = {'fallback': '{}: Top {} Posts In Past {} Days'.format(mentions, args.limit, args.days), 
-                   'pretext': '{}: Top {} Posts In Past {} Days'.format(mentions, args.limit, args.days)}
-elif args.api_type == 'realtime':
-    intro_text = {'fallback': '{}: Top {} Posts In Last {}'.format(mentions, args.limit, args.time), 
-                   'pretext': '{}: Top {} Posts In Last {}'.format(mentions, args.limit, args.time)}
-attachments.append(intro_text)
-for entry in api_results.json()['data']:
-    fields = [{'title': 'Author: {}'.format(entry['author']), 
-                'value': 'Hits: {}'.format(entry['_hits']), 
-                'short':'false'}]
-    temp_dict = {'fallback': '<{}|{}>'.format(entry['url'], entry['title']), 
-                    'pretext':'<{}|{}>'.format(entry['url'], entry['title'])}
-    if not args.no_thumbs:
-        temp_dict['thumb_url'] = entry['thumb_url_medium']
-    if args.shares:
-        shares_result = requests.get(
-            'https://api.parsely.com/v2/shares/post/detail?apikey={}&url={}'.format(
-                APIKEY, entry['url']))
-        shares_dict = {'title': 'shares', 'value': 'Twitter: {}, Facebook: {}'.format(
-                        shares_result.json()['data'][0]['tw'], shares_result.json()['data'][0]['fb']),
-                        'short': 'true'}
-        fields.append(shares_dict)
-    temp_dict['fields'] = fields
-    attachments.append(temp_dict)
-json_payload = {'payload': payload, 'attachments': attachments}
-
-# take our json data and send it up
-try:
-    requests.post(WEBHOOK_URL, data=json.dumps(json_payload))
-except requests.exceptions.MissingSchema:
-    print 'The webhook URL appears to be invalid. Are you sure you have the right webhook URL?'
+class ParselySlack(object):
     
+    def __init__(self, apikey, secret):
+        self._client = Parsely(apikey, secret=secret)
+    
+    def send(attachments=None, channel=None, username=None, text='default text'):
+        ''' send a dict to slack properly '''
+        # use the class objects if they exist
+        slack_channel = channel or self.channel or "General"
+        slack_username = username or self.username or "Parselybot"
+        
+        payload = {
+            'channel': channel, 
+            'username': 'Parselybot', 
+            'text': 'Top 5 posts last 24 hours'}
+            
+        slack_attachments = attachments or self.attachments
+        json_payload = {'payload': payload, 'attachments': slack_attachments}
+
+        # take our json data and send it up
+        try:
+            requests.post(WEBHOOK_URL, data=json.dumps(json_payload))
+            self.attachments = []
+        except requests.exceptions.MissingSchema:
+            print(
+                'The webhook URL appears to be invalid. '
+                'Are you sure you have the right webhook URL?')
+                
+    def build_attachments(entries, text):
+        ''' takes list of Parsely meta objects and makes slack attachments out of them'''
+        attachments = []
+        intro_text = {'fallback': text, 
+                   'pretext': text}
+        attachments.append(intro_text)
+        for entry in entries:
+            meta = entry.__class__.__name__
+            fields = [{
+                'title': '{}: {}'.format(meta, entry.name), 
+                'value': 'Hits: {}'.format(entry['_hits']), 
+                'short':'false'
+                }]
+            if entry.url:
+                temp_dict = {
+                    'fallback': '<{}|{}>'.format(entry.url, 
+                    entry['title']), 
+                    'pretext':'<{}|{}>'.format(entry.url, entry.title)}
+                
+class AnalyticsHandler(object):
+    ''' handles analytics parsing '''
+    
+    def __init__(self, client):
+        self._client = client
+    
+    def parse(self, commands):
+        options = {}
+        ''' takes command (ex. author, John Flynn, monthtodate) and formats it'''
+        # need a better way to do this, have a think later, this is gross
+        
+        if len(command) == 2:
+            # sample command : /parsely, posts, monthtodate
+        parsed['meta'] = command[0].strip()
+        parsed['time'] = command[1].strip()
+        
+        
+        elif len(command) == 3:
+            # sample command: /parsely author, John Flynn, monthtodate
+        parsed['meta'] = command[0].strip()
+        parsed['value'] = command[1].strip()
+        parsed['time'] = command[2].strip()
+        
+        
+        # let's compute some sane things from these
+        if parsed['time'] == 'monthtodate':
+            options['days'] = dt.now().day
+        
+        elif parsed['time'] == 'weektodate':
+            begin_date = dt.now() - datetime.timedelta(dt.now().weekday())
+            options['days'] = begin_date.day
+        
+        elif parsed['time'] == 'today':
+            options['days'] = '1'
+            
+        # have days, let's build query
+        text = 'Top 10 {} in Last {} Days'.format(parsed['meta'], options['days'])
+        post_list = self._client.analytics(aspect=parsed['meta'], **options)
+        return post_list
+        
+        
+        
+        
+            
+            
+    def 
+        
+        
+            

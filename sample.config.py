27b413f8a0e7966401f6f0aacbf516066d74f948
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-02-04 13:25:33 -0500

    added sample.config.py

---
+++ sample.config.py
@@ -0,0 +1,9 @@
+# replace values below with appropriate values as per documentation.
+
+# Parsely API key and shared secret can be found in the dashboard in "Settings"->"API"
+APIKEY = "example.com" # Parsely apikey
+SHARED_SECRET = "abcef12345" # Parsely API shared secret
+TEAM_ID = "T12345" # Team ID (can find at https://api.slack.com/methods/team.info/test)
+SLACK_TOKEN = "abcdef12345" # Slack slash commands integration token 
+LIMIT = 5  # posts to return for each query
+ROOT_URL = "https://api.parsely.com/v2/" # version of the parsely API to use (v2 is most recent as of this commit). Don't change!

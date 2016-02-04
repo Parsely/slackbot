fc31aa916520f16337f0f13173d3d429edcc5a08
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-02-04 07:01:47 -0500

    removed some debug print statements

--- app.py
+++ app.py
@@ -11,18 +11,16 @@ slack = Slack(app)
 slackbot = parsely_slack.ParselySlack(APIKEY, SHARED_SECRET)
 @slack.command('parsely', token=SLACK_TOKEN, team_id=TEAM_ID, methods=['POST']) 
 def parsely(**kwargs):
-    print json.dumps(kwargs)
     text = kwargs.get('text')
     channel = kwargs.get('channel_name')
     commands = [word.strip() for word in text.strip().split(',')]
     if 'author' or 'post' or 'section' or 'tag' or 'referrers' in commands[0]:
         post_list, header_text = slackbot.parse(commands)
-        if text == None:
+        if not text:
             return slack.response("Sorry, didn't recognize that command!")
         if not post_list:
             return slack.response("Sorry, no posts found with that query!")
         attachments = slackbot.build_meta_attachments(post_list, header_text)
-        print json.dumps(attachments)
         return slack.response(text="", response_type="in_channel", attachments=attachments)
     else:
         return slack.response("Sorry, didn't recognize that command!")

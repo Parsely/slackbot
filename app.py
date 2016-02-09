e8c472e46c5dd50e21e31b5c4ca42f797a4a5f87
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-02-09 10:03:35 -0500

    code refactor and cleanup

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 from flask import Flask
-from flask_slack import Slack
-from config import *
+import flask_slack
+import config
 import parsely_slack
 import threading
 import urlparse
 import json
 app = Flask(__name__)
 
-slack = Slack(app)
-slackbot = parsely_slack.ParselySlack(APIKEY, SHARED_SECRET)
-@slack.command('parsely', token=SLACK_TOKEN, team_id=TEAM_ID, methods=['POST'])
-def parsely(**kwargs):
-    text = kwargs.get('text')
-    channel = kwargs.get('channel_name')
+slack = flask_slack.Slack(app)
+slackbot = parsely_slack.ParselySlack(config.APIKEY, config.SHARED_SECRET)
+@slack.command('parsely', token=config.SLACK_TOKEN, team_id=config.TEAM_ID, methods=['POST'])
+def parsely(text=None, channel=None, **kwargs):
     commands = [word.strip() for word in text.strip().split(',')]
-    if 'author' or 'post' or 'section' or 'tag' or 'referrers' in commands[0]:
-        post_list, header_text = slackbot.parse(commands)
-        if not header_text:
-            return slack.response("Sorry, didn't recognize that command!")
-        if not post_list:
-            return slack.response("Sorry, no posts found with that query!")
-        attachments = slackbot.build_meta_attachments(post_list, header_text)
-        return slack.response(text="", response_type="in_channel", attachments=attachments)
-    else:
+    parsed_commands = slackbot.parse(commands)
+    if not parsed_commands:
+        return slack.response("Sorry, I didn't understand that query! Maybe try /parsely help?")
+    post_list, header_text = slackbot.realtime(parsed_commands)
+    if not header_text:
         return slack.response("Sorry, didn't recognize that command!")
-    return slack.response("")
+    if not post_list:
+        return slack.response("Sorry, no posts found with that query!")
+    attachments = slackbot.build_meta_attachments(post_list, header_text)
+    return slack.response(text="", response_type="in_channel", attachments=attachments)
 
 app.add_url_rule('/', view_func=slack.dispatch)
 
--- parsely_slack.py
+++ parsely_slack.py
@@ -7,176 +7,167 @@ import pytz
 import requests
 import tzlocal
 
-from parsely.parsely import Parsely
-from config import *
+from parsely import parsely, models
+import config
 
+class TimePeriod(object):
+    # little time period object to give to realtime client
+    def __init__(self):
+        self.time_str = None
+        self.hours = None
+        self.minutes = None
 
-def string_to_time(time):
-    # turn a string like monthtodate, weektodate, into a time
+def parse_time(time):
+    ''' takes a string like 1h, 2m, and returns a time_period object '''
     # let's compute some sane things from these
-    time_period = lambda: None
-    #python-parsely needs hours set, even if it's blank, or it errors out
-    time_period.hours = []
+    time_period = TimePeriod()
     if time == 'today':
         time_period.hours = int(dt.now(tzlocal.get_localzone()).hour)
-        res = time_period
-        
-    elif time[-1].lower() == 'h':
-        time_period.hours = int(time[:-1])
-        time_period.time_str = 'Hour' if int(time_period.hours) == 1 else 'Hours'
-        res = time_period
-            
-    elif time[-1].lower() == 'm':
-        time_period.minutes = int(time[:-1])
-        if time_period.minutes < 5:
-            # can't do less than 5 minutes, just set to 5
-            time_period.minutes = 5
-        time_period.time_str = 'Minutes'
-        res = time_period
-        
-    elif time[-1].lower() == 'd':
-        if time[:-1].isdigit():
-            res = time[:-1]
-        
-    elif time == 'yesterday':
-        res = '1'
-        
-    elif time.isdigit():
-        res = time
-        
-    else:
-        res = '1'
-    
-    return res
-        
+        return time_period
+
+    if time[-1].lower() == 'h' or 'm' and time[:-1].isdigit():
+        qualifier, time = time[-1].lower(), int(time[:-1])
+        if qualifier == 'h':
+            time_period.time_str = 'Hour' if time == 1 else 'Hours'
+            time_period.hours = time
+        if qualifier == 'm':
+            time_period.time_str = 'Minutes'
+            time_period.minutes = time if time >= 5 else 5
+        return time_period
+
+    return None
+
 class ParselySlack(object):
-    
+
     def __init__(self, apikey, secret, username=None, password=None):
-        self._client = Parsely(apikey, secret=secret)
+        self._client = parsely.Parsely(apikey, secret=secret)
         # pull default config params
-        self.config = {'limit': LIMIT} 
-                
+
     def build_meta_attachments(self, entries, text):
-        ''' takes list of Parsely meta objects and makes slack attachments out of them'''
+        ''' takes list of Parsely meta objects and makes slack attachments out of them '''
         attachments = [{'fallback': text, 'pretext': text}]
         for index, entry in enumerate(entries):
-            if entry.__class__.__name__ == 'Post':
+            if isinstance(entry, models.Post):
                 attachment = self.build_post_attachment(index, entry)
                 attachments.append(attachment)
             else:
                 attachments.append(self.build_meta_attachment(index, entry))
         return attachments
-        
+
     def build_post_attachment(self, index, entry):
         ''' takes one post and builds an attachment for it '''
-        title = str(index+1) + '. ' + entry.title
-        dash_link = self.get_dash_link(entry.url)
+        title = "%s. %s" % (str(index+1), entry.title)
+        dash_link = self.get_dash_link(entry)
         res_list = [{
-                'value': 'Hits: {}'.format(entry.hits), 
+                'value': 'Hits: %s' % entry.hits,
                 'short':'false',
-                'title': 'Author: {}'.format(entry.author)
+                'title': 'Author: %s' % entry.author
                 }]
         attachment = {
-            'fallback': '<{}|{}>'.format(dash_link, title), 
-            'pretext':'<{}|{}>'.format(dash_link, title),
+            'fallback': '<%s|%s>' % (dash_link, title),
+            'pretext':'<%s|%s>' % (dash_link, title),
             'thumb_url': entry.thumb_url_medium
             }
         shares = self._client.shares(post=entry.url)
         shares_dict = {
-            'title': 'shares', 'value': 'Twitter: {}, Facebook: {}'.format(
-                shares.twitter, shares.facebook),
-                'short': 'true'}
+            'title': 'shares',
+            'value': 'Twitter: %s, Facebook: %s' % (shares.twitter, shares.facebook),
+            'short': 'true'}
         res_list.append(shares_dict)
         if entry.visitors:
             visitors_dict = {
                 'title': 'visitors',
-                'value': '{}'.format(entry.visitors),
+                'value': '%s' % entry.visitors,
                 'short': 'true'}
             res_list.append(visitors_dict)
         attachment['fields'] = res_list
         return attachment
-            
+
     def build_meta_attachment(self, index, entry):
-        title = str(index+1) + '. ' + entry.name
-        value_url_string = entry.name.replace(' ', '_')
-        meta_url_string = "{}s".format(entry.__class__.__name__)
-        url = 'http://dash.parsely.com/{}/{}/{}/'.format(APIKEY, meta_url_string.lower(), value_url_string)
         fields = [{
-                'value': 'Hits: {}'.format(entry.hits), 
+                'value': 'Hits: %s' % entry.hits,
                 'short':'false'
                 }]
         try:
-            fields[0]['value'] += "\nReferrer Type: {}".format(entry.ref_type)
-            if entry.ref_type == 'direct':
-                url = 'http://dash.parsely.com/{}/referrers/'.format(APIKEY)
-            if entry.ref_type == 'self':
-                url = 'http://dash.parsely.com/{}/{}/internal/{}'.format(APIKEY, meta_url_string.lower(), value_url_string)
-            else:
-                url = 'http://dash.parsely.com/{}/{}/{}/{}'.format(APIKEY, meta_url_string.lower(), entry.ref_type, value_url_string)
+            fields[0]['value'] += "\nReferrer Type: %s" % entry.ref_type
         except AttributeError:
             pass
+        url = self.get_dash_link(entry)
         attachment = {
-            'fallback': '<{}|{}>'.format(url, entry.name), 
+            'fallback': '<{}|{}>'.format(url, entry.name),
             'pretext':'<{}|{}>'.format(url, entry.name)}
         attachment['fields'] = fields
         return attachment
-    
-    def get_dash_link(self, url):
-        ''' gets a dash link for the given URL '''
-        return "http://dash.parsely.com/{}/find?url={}".format(APIKEY, url)
-        
+
+    def get_dash_link(self, entry):
+        ''' gets a dash link for the given Parsely model object '''
+        if isinstance(entry, models.Post):
+            return "http://dash.parsely.com/%s/find?url=%s" % (config.APIKEY, entry.url)
+        else:
+            value_url_string = entry.name.replace(' ', '_')
+            meta_url_string = "%ss" % (entry.__class__.__name__)
+            if not isinstance(entry, models.Referrer):
+                return 'http://dash.parsely.com/%s/%s/%s/' % (config.APIKEY, meta_url_string.lower(), value_url_string)
+            if entry.ref_type == 'direct':
+                return 'http://dash.parsely.com/%s/referrers/' % (config.APIKEY)
+            if entry.ref_type == 'self':
+                return 'http://dash.parsely.com/%s/%s/self/%s' % (config.APIKEY, meta_url_string.lower(), value_url_string)
+            else:
+                return 'http://dash.parsely.com/%s/%s/%s/%s' % (config.APIKEY, meta_url_string.lower(), entry.ref_type, value_url_string)
+
+
+
     def parse(self, commands):
-        parsed, options = {}, {}
-        options['limit'] = LIMIT
         ''' takes command (ex. author, John Flynn, monthtodate) and formats it'''
-        metas = ["posts", "tags", "sections", "authors"]
+        parsed = {}
+        if len(commands) < 2:
+            return None
+        command, params = commands[0].strip(), [x.strip() for x in commands[1:]]
+        metas = ["posts", "tags", "sections", "authors", "referrers"]
         metas_detail = ["tag", "section", "author"]
-        if not commands[0] in metas:
+        if not command in metas:
             return None, None
-        if commands[0] == 'referrers':
-            parsed['meta'] = commands[0]
-            parsed['time'] = commands[1]
-        elif commands[0] in metas:
+        elif command in metas:
             # sample command : /parsely, posts, monthtodate
             if len(commands) == 1:
                 if not parsed.get('time'):
                 # give default of last hour
                     parsed['time'] = '1h'
-                parsed['meta'] = commands[0].strip()
+                parsed['meta'] = command.strip()
             if len(commands) == 2:
-                parsed['meta'] = commands[0].strip()
-                parsed['time'] = commands[1].strip()
+                parsed['meta'] = command.strip()
+                parsed['time'] = params[0].strip()
             if len(commands) == 3:
-                parsed['meta'] = commands[0].strip()
-                parsed['value'] = commands[1].strip() if commands[1] in metas_detail else None
-                parsed['time'] = commands[2].strip()
-        
+                parsed['meta'] = command.strip()
+                parsed['value'] = params[0].strip() if params[0] in metas_detail else None
+                parsed['time'] = params[0].strip()
+
         else:
             return None, None
 
-        return self.realtime(parsed, **options)
-        
-        
+        return parsed
+
+
     def realtime(self, parsed, **kwargs):
         # takes parsed commands and returns a post_list for realtime
-        time_period = string_to_time(parsed['time'])
+        time_period = parse_time(parsed['time'])
         filter_string, time_string = "", ""
         if parsed.get('value'):
             filter_meta, value = [x.strip() for x in parsed['value'].split(':')]
             kwargs[filter_meta] = value
-            filter_string = "For {} {}".format(filter_meta, value)
-        post_list = self._client.realtime(aspect=parsed['meta'], per=time_period, **kwargs)
+            filter_string = "For %s %s" % (filter_meta, value)
+        post_limit = 50 if parsed.get('meta') == 'referrers' else config.LIMIT
+        post_list = self._client.realtime(aspect=parsed['meta'], per=time_period, limit=post_limit, **kwargs)
         if parsed['meta'] == 'referrers':
             post_list.sort(key=lambda x: x.hits, reverse=True)
             # for some reason realtime referrers doesn't honor limit
-            post_list = post_list[:LIMIT]
+            post_list = post_list[:config.LIMIT]
         if parsed['time'] == 'today':
             time_string = "Today"
         else:
-            time_string = "In Last {} {}".format(parsed['time'][:-1], str(time_period.time_str))
-        text = "Top {} {} {} {}".format(str(len(post_list)), parsed['meta'], filter_string, time_string)
+            time_string = "In Last %s %s" % (parsed['time'][:-1], str(time_period.time_str))
+        text = "Top %s %s %s %s" % (str(len(post_list)), parsed['meta'], filter_string, time_string)
         return post_list, text
-        
-        
-        
-        
+
+
+

b8f82f1e38e7fbb19feb1706b316894dd5ab99c2
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-02-12 09:04:30 -0500

    changed license, renamed readme and travis

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Parsely',
     author_email='support@parsely.com',
     scripts=['bin/parsely_slackbot'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('Readme.md').read(),
+    license='Apache License 2.0',
+    long_description=open('README.rst').read(),
     install_requires=[str(req.req) for req in parse_requirements('requirements.txt', session=uuid.uuid1())]
 )
